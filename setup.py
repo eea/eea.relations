8b3cd78f0c15c99279ccc0fb1532810ed38e6df1
voineali <voineali@24821446-e7ea-0310-81ac-b189f4507b0b>
2010-04-22 10:22:58 +0000

    #3272 Rename

--- setup.py
+++ setup.py
@@ -3,23 +3,22 @@ import os
 
 version = '0.1'
 
-setup(name='eea.widget.referencebrowser',
+setup(name='eea.relations',
       version=version,
-      description="EEA custom archetypes reference browser widget",
+      description="EEA Possible Relations",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
-      keywords='eea widget reference browser referencebrowserwidget faceted facetednavigation plone zope python',
+      keywords='eea relations widget reference browser referencebrowserwidget faceted facetednavigation plone zope python',
       author='Alin Voinea',
       author_email='alin.voinea@eaudeweb.ro',
       url='http://eea.europa.eu',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['eea', 'eea.widget'],
+      namespace_packages=['eea',],
       include_package_data=True,
       zip_safe=False,
       install_requires=[
