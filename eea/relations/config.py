c33116be3b9338e0ee1922bae96fdaecc1e88211
voineali <voineali@24821446-e7ea-0310-81ac-b189f4507b0b>
2010-04-23 17:10:41 +0000

    #3267 Look for graphviz installation in {buildout:directory}/parts/graphviz

--- eea/relations/config.py
+++ eea/relations/config.py
@@ -1,6 +1,6 @@
 """ Config
 """
-
+from graph import GRAPHVIZ_PATHS
 PROJECTNAME = 'eea.relations'
 ADD_CONTENT_PERMISSION = "Add portal content"
 SUBTYPE = 'eea.relations.facetednavigation.ContentTypeFacetedNavigable'
--- eea/relations/graph/graph.py
+++ eea/relations/graph/graph.py
@@ -3,8 +3,8 @@
 import os
 from tempfile import mktemp
 from zope.interface import implements
-
 from interfaces import IGraph
+from eea.relations.config import GRAPHVIZ_PATHS
 
 class Graph(object):
     """ Generates a PNG graph
@@ -17,11 +17,15 @@ class Graph(object):
     def __call__(self, graph):
         """ Draw pydot.Graph
         """
+        if GRAPHVIZ_PATHS:
+            graph.progs = GRAPHVIZ_PATHS
+
         writter = getattr(graph, 'write_' + self.format, None)
         if not writter:
             return None
 
         path = mktemp('.%s'  % self.format)
+        prog = GRAPHVIZ_PATHS
         img = writter(path=path)
 
         img = open(path, 'rb')
