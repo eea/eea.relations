f716edb41eec02333baa9554d2cea217bd5c529c
voineali <voineali@24821446-e7ea-0310-81ac-b189f4507b0b>
2010-04-26 12:33:44 +0000

    #3277 Auto generate relation's title.

--- eea/relations/content/relation.py
+++ eea/relations/content/relation.py
@@ -3,11 +3,55 @@
 from zope.interface import implements
 from Products.Archetypes import atapi
 from Products.ATContentTypes.content.folder import ATFolder
+from Products.CMFCore.utils import getToolByName
 from eea.facetednavigation.widgets.field import StringField
 
 from interfaces import IRelation
 
+class TitleWidget(atapi.StringWidget):
+    """ Auto generate title
+    """
+    def process_form(self, instance, field, form, empty_marker=None,
+                     emptyReturnsMarker=False, validating=True):
+        """ Process form
+        """
+        ct_from = form.get('from', '')
+        ct_to = form.get('to', '')
+        if not (ct_from and ct_to):
+            return '', {}
+
+        rtool = getToolByName(instance, 'portal_relations')
+        brains = rtool.getFolderContents(contentFilter={
+            'portal_type': 'EEARelationsContentType',
+            'getId': ct_from
+        })
+        for brain in brains:
+            ct_from = brain.Title
+            break
+
+        brains = rtool.getFolderContents(contentFilter={
+            'portal_type': 'EEARelationsContentType',
+            'getId': ct_to
+        })
+        for brain in brains:
+            ct_to = brain.Title
+            break
+
+        value = '%s -> %s' % (ct_from, ct_to)
+        return value, {}
+
 EditSchema = ATFolder.schema.copy() + atapi.Schema((
+    atapi.StringField(
+        name='title',
+        required=0,
+        searchable=1,
+        accessor='Title',
+        widget=TitleWidget(
+            label_msgid='label_title',
+            modes=(),
+            i18n_domain='plone',
+        ),
+    ),
     StringField('from',
         schemata="default",
         vocabulary_factory='eea.relations.voc.ContentTypes',
