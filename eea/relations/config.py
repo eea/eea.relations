61e7f2f6469a0c7678f1cd4f638cc2e270dbe4ce
voineali <voineali@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-04-22 14:28:46 +0000

    Refs #4232   - More cleanup, code and docs update.

--- eea/relations/config.py
+++ eea/relations/config.py
@@ -3,4 +3,3 @@
 from eea.relations.graph import GRAPHVIZ_PATHS
 PROJECTNAME = 'eea.relations'
 ADD_CONTENT_PERMISSION = "Add portal content"
-GLOBALS = globals()
--- eea/relations/tests/base.py
+++ eea/relations/tests/base.py
@@ -5,8 +5,6 @@ from Products.Five import fiveconfigure
 from Products.PloneTestCase import PloneTestCase
 from Products.PloneTestCase.layer import onsetup
 
-PloneTestCase.installPackage('eea.relations')
-
 @onsetup
 def setup_eea_relations():
     """Set up the additional products. """
@@ -15,10 +13,10 @@ def setup_eea_relations():
     zcml.load_config('configure.zcml', eea.relations)
     fiveconfigure.debug_mode = False
 
+    PloneTestCase.installPackage('eea.relations')
+
 setup_eea_relations()
-PloneTestCase.setupPloneSite(extension_profiles=(
-    'eea.relations:default', 'eea.relations:demo'
-))
+PloneTestCase.setupPloneSite(extension_profiles=('eea.relations:demo',))
 
 class EEARelationsFunctionalTestCase(PloneTestCase.FunctionalTestCase):
     """ Base class for functional tests for the 'EEA Relations' product.
