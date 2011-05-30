edf63db2da6da644946a7c0b89dbe77f0ba19e72
tiberich <tiberich@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-05-30 13:31:54 +0000

    Refs #4305; eea.workflow doesn't have this interface

--- eea/relations/field/interfaces.py
+++ eea/relations/field/interfaces.py
@@ -2,16 +2,10 @@
 """
 from zope.interface import Interface
 
-try:
-    from eea.workflow import interfaces
-    IValueProvider = interfaces.IValueProvider
-    IRequiredFor = interfaces.IRequiredFor
-except ImportError:
-    # eea.workflow is not mandatory
-    class IValueProvider(Interface):
-        """ Value provider """
-        pass
+class IValueProvider(Interface):
+    """ Value provider """
+    pass
 
-    class IRequiredFor(Interface):
-        """ Required for """
-        pass
+class IRequiredFor(Interface):
+    """ Required for """
+    pass
