2cc3ec85faa090b6bb4f7f12cf17b477887fc893
ghicaale <ghicaale@24821446-e7ea-0310-81ac-b189f4507b0b>
2012-09-19 16:16:37 +0000

    Results sorted alphabetically by title. Added warning message. Added more informations on the results report. refs #3212

--- eea/relations/browser/app/badrelations.py
+++ eea/relations/browser/app/badrelations.py
@@ -5,6 +5,7 @@ from Products.Five.browser import BrowserView
 from Products.CMFCore.utils import getToolByName
 from eea.relations.interfaces import IToolAccessor
 from eea.relations.component import getForwardRelationWith
+from Products.statusmessages.interfaces import IStatusMessage
 import logging
 
 logger = logging.getLogger('eea.relations.browser.badrelations')
@@ -12,6 +13,17 @@ logger = logging.getLogger('eea.relations.browser.badrelations')
 class View(BrowserView):
     """ Views
     """
+    def __call__(self):
+        """ View
+        """
+        status = queryAdapter(self.request, IStatusMessage)
+        if status:
+            status.addStatusMessage('Be aware that this operation can \
+                take a long time to perform if a lot of objects are \
+                involved.', type='info')
+
+        return self.index()
+
     @property
     def content_types(self):
         """ Content types
@@ -24,26 +36,39 @@ class View(BrowserView):
             yield brain
 
     @property
-    def bad_relations_report(self):
-        """ All relations
+    def get_objects(self):
+        """ Get objects
         """
-        res = []
         query = {}
-        report = {}
+        res = []
 
         catalog = getToolByName(self.context, 'portal_catalog')
         pr_tool = getToolByName(self.context, 'portal_relations')
+
         ct_type = self.request.get('ct_type', '')
 
-        # Get portal relations content type
         if ct_type:
             pr_ct_type = pr_tool[ct_type]
 
+            query['sort_on'] = 'sortable_title'
             if pr_ct_type.getCt_type():
                 query['portal_type'] = pr_ct_type.getCt_type()
             if pr_ct_type.getCt_interface():
                 query['object_provides'] = pr_ct_type.getCt_interface()
             res = catalog(**query)
+
+        return res
+
+    @property
+    def bad_relations_report(self):
+        """ Get bad relations
+        """
+        logger.info('Start generating bad relations report.')
+        res = []
+        report = []
+
+        # Get portal relations content type
+        res = self.get_objects
 
         # Get relations
         for brain in res:
@@ -56,7 +81,7 @@ class View(BrowserView):
                 for rel in fwd:
                     if not getForwardRelationWith(obj, rel):
                         bad_relations.append(rel)
-                report[obj] = bad_relations
+                report.append((obj.Title(), obj, bad_relations))
             except (TypeError, ValueError):
                 # The catalog expects AttributeErrors when
                 # a value can't be found
@@ -64,4 +89,6 @@ class View(BrowserView):
             except:
                 logger.info('ERROR getting relations for %s' % brain.getURL())
 
+        report.sort()
+        logger.info('Done generating bad relations report.')
         return report
