60e42079f9d8884843367db041069f633a5f0214
ichim-david <ichim.david@gmail.com>
2012-11-21 12:01:58 +0200

    Refs #9547; Added subcriber to update relations after a content type has been renamed

--- eea/relations/subtypes/events.py
+++ eea/relations/subtypes/events.py
@@ -9,6 +9,9 @@ from eea.relations.subtypes.interfaces import (
     IOriginalFacetedNavigable,
     IFacetedNavigable,
 )
+from eea.relations.interfaces import IToolAccessor
+from Products.CMFCore.utils import getToolByName
+from zope.component import queryAdapter
 
 def subtype(obj, evt):
     """ Subtype as faceted navigable
@@ -48,3 +51,33 @@ def faceted_enabled(doc, evt):
 
     noLongerProvides(doc, IOriginalFacetedNavigable)
     alsoProvides(doc, IFacetedNavigable)
+
+def object_renamed(obj, evt):
+    """ EVENT: EEARelationsContentType object renamed
+    """
+    # if there is no newName then it means that we don't really have a renamed
+    # object event so don't do anything
+    if not evt.newName:
+        return
+    tool = queryAdapter(obj, IToolAccessor)
+    relations = tool.relations(proxy=False)
+    pt_relations = getToolByName(obj, 'portal_relations')
+    bad_rel = ""
+    # rename relations after a EEARelationsContentType rename to avoid broken
+    # relations
+    for relation in relations:
+        from_rel = relation['from']
+        to_rel = relation['to']
+        pr_from = pt_relations.get(from_rel)
+        pr_to = pt_relations.get(to_rel)
+        if not pr_from:
+            bad_rel = from_rel
+            # rename only relation that is equal to the old id
+            if bad_rel == evt.oldName:
+                from_field = relation.getField('from')
+                from_field.set(relation, evt.newName)
+        if not pr_to:
+            bad_rel = to_rel
+            if bad_rel == evt.oldName:
+                to_field = relation.getField('to')
+                to_field.set(relation, evt.newName)
