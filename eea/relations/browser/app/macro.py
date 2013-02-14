4fa3f25a792d5ce14988718481e6fa8d85b608ec
ichim-david <ichim.david@gmail.com>
2013-02-14 14:30:01 +0200

    Optimized backward relation check to do it's checking only once per portal_type

--- eea/relations/browser/app/macro.py
+++ eea/relations/browser/app/macro.py
@@ -59,15 +59,49 @@ class Macro(BrowserView):
         relation = kwargs.get('relation', 'relatesTo')
 
         relations = getBRefs(relation)
+        contentTypes = {}
+        nonBackwardRelations = []
+        if relations:
+            # save the name and the portal type of the first relation that we
+            # have permission to use.
+            # this way we can check if other relations are of same portal_type
+            # if they are then we don't need to check if it's a backward
+            # relation and what is it's name, we can just add it to the tabs
+            # for that relation name the relation item
+            for relation in relations:
+                if self.checkPermission(relation):
+                    firstRelation = relation
+                    backward = getBackwardRelationWith(self.context,
+                                                                  firstRelation)
+                    if not backward:
+                        return tabs
+                    name = backward.getField('backward_label').getAccessor(
+                                                                     backward)()
+                    # save for the portal_type the resulted backwards
+                    # relation name
+                    contentTypes[firstRelation.portal_type] = name
+                    break
+
         for relation in relations:
             if not self.checkPermission(relation):
                 continue
+            portalType = relation.portal_type
+            # if the portal_type of the relation is not already in
+            # contentTypes than we are dealing with a backward relation that
+            # is different from the ones we had before therefore we need
+            if portalType not in contentTypes:
+                # don't check if relation is backward if the portal_type has
+                # already been identified as nonBackward
+                if portalType in nonBackwardRelations:
+                    continue
+                backward = getBackwardRelationWith(self.context, relation)
+                if not backward:
+                    nonBackwardRelations.append(portalType)
+                    continue
+                name = backward.getField('backward_label').getAccessor(backward)()
+                contentTypes[portalType] = name
+            name = contentTypes[portalType]
 
-            backward = getBackwardRelationWith(self.context, relation)
-            if not backward:
-                continue
-
-            name = backward.getField('backward_label').getAccessor(backward)()
             if name not in tabs:
                 tabs[name] = []
             tabs[name].append(relation)
