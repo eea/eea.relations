e94eab59bfa2c51c08fa7f9aed11e728fd37a384
ichim-david <ichim.david@gmail.com>
2013-04-10 18:21:40 +0300

    Refs #13830 EEA; Changed the following: - Removed the sorting plugin logic since we can give the sorting logic   to the sort method on the jquery collection - Removed metadatata div and added the metadata as data attributes on   the listing objects - Added code to sort the elements by title and effective date both in   the a general context but also that of EEA

--- eea/relations/skins/eea_relations/eea_relations.js
+++ eea/relations/skins/eea_relations/eea_relations.js
@@ -1,60 +1,40 @@
-// jquery plugin from
-// http://james.padolsey.com/javascript/sorting-elements-with-jquery/
-jQuery.fn.sortElements = (function(){
-
-    var sort = [].sort;
-
-    return function(comparator, getSortable) {
-
-        getSortable = getSortable || function(){return this;};
-
-        var placements = this.map(function(){
-
-            var sortElement = getSortable.call(this),
-                parentNode = sortElement.parentNode,
-
-                // Since the element itself will change position, we have
-                // to have some way of storing its original position in
-                // the DOM. The easiest way is to have a 'flag' node:
-                nextSibling = parentNode.insertBefore(
-                    document.createTextNode(''),
-                    sortElement.nextSibling
-                );
-
-            return function() {
-
-                if (parentNode === this) {
-                    throw new Error(
-                        "You can't sort elements if any one is a descendant of another."
-                    );
-                }
-
-                // Insert before flag:
-                parentNode.insertBefore(this, nextSibling);
-                // Remove flag:
-                parentNode.removeChild(nextSibling);
-
-            };
-
-        });
-
-        return sort.call(this, comparator).each(function(i){
-            placements[i].call(getSortable.call(this));
-        });
-
-    };
-
-})();
-
 jQuery(function($){
     var relations = $('#relatedItems').find('.visualNoMarker > div');
-    // 13870 sort relations based on given criterias
+    var tab_panels = $(".eea-tabs-panel");
+    // 13870 sort relations based on given criteria
     $("select[name='sort_by']").change(function(e) {
-        var sort_parameter = '.' + e.currentTarget.value;
+        var sort_parameter = e.currentTarget.value;
         relations.each(function(){
-            $(this).children().sortElements(function(a, b) {
-                return $(a).find(sort_parameter).text() > $(b).find(sort_parameter).text() ? 1 : -1;
+            var $this = $(this);
+            var $children = $this.children().detach();
+            // sort based on the data attributes set on the listing elements
+            $children.sort(function(a, b) {
+                return $(a).data(sort_parameter) > $(b).data(sort_parameter) ? 1 : -1;
+            });
+            $this.append($children);
+        });
+        $(window).trigger('relations.sort', sort_parameter);
+    });
+
+    $(window).bind('relations.sort', function(ev, sort_parameter) {
+        tab_panels.each(function(){
+            // sort items differently if we have eea-tabs present
+            // this event can be bound by third party code which can supplement different
+            // sorting
+            var $this = $(this);
+            var $listing_entries = $this.find('.photoAlbumEntry, .tileItem').detach();
+            $listing_entries.sort(function(a, b) {
+                return $(a).data(sort_parameter) > $(b).data(sort_parameter) ? 1 : -1;
+            });
+            var slice_index = 0;
+            $('.page', $this).each(function(i, el){
+                var $el = $(el);
+                var count = $el.data('count');
+                var current_index = slice_index;
+                slice_index = slice_index + count;
+                $el.append($listing_entries.slice(current_index, slice_index));
             });
         });
     });
+
 });
