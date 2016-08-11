1d0976c50d237402d0d631accfe1bbc2da031c03
Catalin Mititiuc <mititiuc.cata@gmail.com>
2016-08-11 18:25:28 +0300

    refs #70758 - update interface

--- eea/relations/events/interfaces.py
+++ eea/relations/events/interfaces.py
@@ -11,22 +11,8 @@ class IEvent(IObjectEvent):
     """ Base Event Interface for all export events
     """
 
-class IExportSuccess(IEvent):
-    """ Export succeeded
-    """
-
-class IExportFail(IEvent):
-    """ Export failed
-    """
-
-class IAsyncEvent(IEvent):
+class IRelatedItemsWorkflowStateChanged(IEvent):
     """ Base Event Interface for all Async events
     """
 
-class IAsyncExportSuccess(IAsyncEvent):
-    """ Async job for export succeeded
-    """
 
-class IAsyncExportFail(IAsyncEvent):
-    """ Async job for export failed
-    """
