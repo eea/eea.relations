27589eebd1bc93250eef62d379e3733b8b0e898e
Catalin Mititiuc <mititiuc.cata@gmail.com>
2016-08-11 18:24:01 +0300

    refs #70758 - init update

--- eea/relations/events/__init__.py
+++ eea/relations/events/__init__.py
@@ -1,10 +1,23 @@
 """ Custom events
 """
-from zope.interface import implements
+from zope.interface import implements, implementer
 from zope.lifecycleevent import ObjectModifiedEvent
 from eea.relations.events.interfaces import IObjectInitializedEvent
+from eea.relations.events.interfaces import IEvent, IRelatedItemsWorkflowStateChanged
 
 class ObjectInitializedEvent(ObjectModifiedEvent):
     """ An object is being initialised, i.e. populated for the first time
     """
     implements(IObjectInitializedEvent)
+
+@implementer(IEvent)
+class Event(object):
+    """ Abstract event
+    """
+    def __init__(self, context, **kwargs):
+        self.object = context
+
+@implementer(IRelatedItemsWorkflowStateChanged)
+class AsyncEvent(Event):
+    """ Abstract event for all async events
+    """
