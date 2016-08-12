b7cf76a5aab7998ec111f8605c64fa6c8a7701f4
Catalin Mititiuc <mititiuc.cata@gmail.com>
2016-08-12 15:43:39 +0300

    refs #70758 - add forward and backward

--- eea/relations/events/__init__.py
+++ eea/relations/events/__init__.py
@@ -3,21 +3,27 @@
 from zope.interface import implements, implementer
 from zope.lifecycleevent import ObjectModifiedEvent
 from eea.relations.events.interfaces import IObjectInitializedEvent
-from eea.relations.events.interfaces import IEvent, IRelatedItemsWorkflowStateChanged
+from eea.relations.events.interfaces import IRelatedItemsWorkflowStateChanged
+from eea.relations.events.interfaces import IForwardRelatedItemsWSC
+from eea.relations.events.interfaces import IBackwardRelatedItemsWSC
 
 class ObjectInitializedEvent(ObjectModifiedEvent):
     """ An object is being initialised, i.e. populated for the first time
     """
     implements(IObjectInitializedEvent)
 
-@implementer(IEvent)
-class Event(object):
-    """ Abstract event
+@implementer(IRelatedItemsWorkflowStateChanged)
+class RelatedItemsWorkflowStateChanged(object):
+    """ Related Items Workflow State Changed
     """
-    def __init__(self, context, **kwargs):
+    def __init__(self, context):
         self.object = context
 
-@implementer(IRelatedItemsWorkflowStateChanged)
-class AsyncEvent(Event):
-    """ Abstract event for all async events
+@implementer(IForwardRelatedItemsWSC)
+class ForwardRelatedItemsWorkflowStateChanged(RelatedItemsWorkflowStateChanged):
+    """ Related Items Workflow State Changed
+    """
+@implementer(IBackwardRelatedItemsWSC)
+class BackwardRelatedItemsWorkflowStateChanged(RelatedItemsWorkflowStateChanged):
+    """ Related Items Workflow State Changed
     """
